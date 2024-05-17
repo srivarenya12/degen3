75029bee1daa706414dc13e452b3cc6219c27a35
srivarenya12 <onlysrivarenya@gmail.com>
2024-05-17 21:07:24 +0530

    commit 1

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -2,13 +2,38 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";  
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    constructor() ERC20("Degen", "DGN") {}
+    string public redeem_shop;
+    string internal error_m = "Only 3 items available in the shop";
+
+    constructor() ERC20("Degen", "DGN") {
+        redeem_shop = "You have the following items in your shop which you are able to redeem: 1.toys, 2.places, 3.curtains";
+    }
 
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
-    }
+        }
+
+        function burn(uint256 amount) public override{ 
+            _burn(msg.sender, amount);
+        }
+
+        function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+            _transfer(_msgSender(), recipient, amount);
+            return true;
+        }
+        
+        function redeem(uint256 item_serial) public payable{
+        
+        if(item_serial < 0 && item_serial > 4){
+            revert(error_m);
+        }
+        require(balanceOf(msg.sender) >= 10);
+        _burn(msg.sender, (item_serial*2));
+        }
+        
 }
